706146c4f8ab34ba0d8af9efe74f57f91e8df762
Camilo Gonzalez <milogonzm@gmail.com>
2021-11-27 14:16:17 -0300

    Entregable

--- Clase02/js/index.js
+++ Clase02/js/index.js
@@ -1,2 +1,8 @@
-let ingreso1 = prompt("Ingresa 1 dato");
-let ingreso2 = prompt("Ingresa otro dato");
+alert("Estoy pensando en un número del 1 al 10");
+let ingreso = prompt("Adivina el número!");
+if (ingreso == 7) {
+    alert("Adivinaste!!!")
+} else {
+    alert("No era ese :C. Recarga para intentar nuevamente")
+}
+
