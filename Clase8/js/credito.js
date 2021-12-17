c0d08c988dd58aa6adc9c84f173f172a1fb60a63
Camilo Gonzalez <milogonzm@gmail.com>
2021-12-16 23:05:52 -0300

    Trabajo de interracion HTML, con modificacion de DOM

--- Clase8/js/credito.js
+++ Clase8/js/credito.js
@@ -1,34 +1,58 @@
+export function calcular(){     
+    let nombre = document.getElementById('nombre').value
+    let montoCredito = document.getElementById('monto').value;
+    let cuotaCredito = JSON.parse(document.getElementById('cuotas').value);
+    const credito1 = new Credito (nombre,montoCredito,cuotaCredito);
+    console.log(credito1)
+    if( nombre == null || nombre.length == 0 || !/^[A-Za-z]+$/.test(nombre) ){
+        alert("Ingresaste un nombre invalido. Intenta de nuevo")
+    } else if(montoCredito<10000){
+        alert("El credito debe ser de $10.000 como minimo")
+    } else if(isNaN(montoCredito) || montoCredito=="0" || montoCredito==""){
+        alert("Ingresaste un monto invalido. Intenta de nuevo")
+    } else if(cuotaCredito == 3){
+        credito1.interes=JSON.parse(localStorage.getItem('interes1'));
+        credito1.calculoCredito1();
+        credito1.mostrarCredito();
+    } else if (cuotaCredito == 6){
+        credito1.interes=JSON.parse(localStorage.getItem('interes2'));
+        credito1.calculoCredito2();
+        credito1.mostrarCredito();
+    } else {   
+        credito1.interes=JSON.parse(localStorage.getItem('interes3'));
+        credito1.calculoCredito3();
+        credito1.mostrarCredito();
+    }
+}  
 
 export class Credito{
-    constructor(montoCredito,cuotaCredito,interes) {
+    constructor(nombre,montoCredito,cuotaCredito,interes) {
+        this.nombre = nombre;
         this.montoCredito = montoCredito;
         this.cuotaCredito = cuotaCredito; 
         this.interes = interes;
     }
     calculoCredito1() {
-        console.log("El monto solicitado es de $" + this.montoCredito)
         this.monto =  this.montoCredito * this.interes;
         this.cuota = this.monto / this.cuotaCredito;
     }
     calculoCredito2() {
-        console.log("El monto solicitado es de $" + this.montoCredito)
         this.monto =  this.montoCredito * this.interes;
         this.cuota = this.monto / this.cuotaCredito;
     }
     calculoCredito3() {
-        console.log("El monto solicitado es de $" + this.montoCredito)
         this.monto =  this.montoCredito * this.interes;
         this.cuota = this.monto / this.cuotaCredito;
     }
+    mostrarCredito() {
+        this.monto = this.monto.toFixed(0)
+        this.cuota = this.cuota.toFixed(0)
+        const mostrar = document.createElement("DIV");
+        mostrar.className = 'mostrar'
+        mostrar.innerHTML =     `<h2> ${this.nombre}, tu crédito es el siguiente:</h2>
+                                <h3> Si solicitas $${this.montoCredito} en ${this.cuotaCredito} cuotas</h3>
+                                <h4> Cada una será de $${this.cuota}</h4>
+                                <h5> Por un total de $${this.monto}</h5>`;
+        document.querySelector(".credit__contenido").appendChild(mostrar);
+    }
 }
-
-
-/* const calculo =  document.getElementById(submit)
-calculo.addeventlistener("submit", function() {
-    funcicon()
-})
-
-function funcicon() {
-    monto = montoCredito;
-} */
-
--- Clase8/js/main.js
+++ Clase8/js/main.js
@@ -1,63 +1,11 @@
 import { Credito } from "./credito.js";
+import { calcular } from "./credito.js"
 
 localStorage.setItem('interes1', 1.15);
 localStorage.setItem('interes2', 1.20);
 localStorage.setItem('interes3', 1.30);
 
-function calcular(){     
-    console.log('Hola!');
-    let montoCredito = document.getElementById('monto').value;
-    let cuotaCredito = JSON.parse(document.getElementById('cuotas').value);
-    const credito1 = new Credito (montoCredito,cuotaCredito);
-    console.log(credito1)
-    if (cuotaCredito == 3){
-        credito1.interes=JSON.parse(localStorage.getItem('interes1'));
-        credito1.calculoCredito1();
-    } else if (cuotaCredito == 6){
-        credito1.interes=JSON.parse(localStorage.getItem('interes2'));
-        credito1.calculoCredito2();
-    } else {   
-        credito1.interes=JSON.parse(localStorage.getItem('interes3'));
-        credito1.calculoCredito3();
-    }
-}  
-
 const simula=document.getElementById('submit')
 simula.addEventListener("click", function() { 
     calcular();
 })
-
-/* let creditos = []   /* Define Array vacio 
-console.log("Bienvenid@ a nuestro cotizador de creditos")
-alert("Bienvenid@ a nuestro cotizador de creditos")
-let montoCredito = prompt("Cuanto dinero necesitas? El minimo a solicitar es de $10.000")
-
-if(montoCredito<10000){
-    alert("El credito debe ser de $10.000 como minimo")
-} else if(isNaN(montoCredito) || montoCredito=="0" || montoCredito==""){
-    alert("Ingresaste un monto invalido. Intenta de nuevo recargando la pagina")
-    } else {
-        let cuotaCredito = prompt("Ingresa la cantidad de cuotas que deseas (3, 6 o 12)")
-        if(cuotaCredito==3){
-            const credito1 = new Credito (montoCredito,cuotaCredito);
-            credito1.calculoCredito1();
-            creditos.push(credito1);   /* Actualiza array 
-        } else if(cuotaCredito==6) {
-            const credito1 = new Credito (montoCredito,cuotaCredito);
-            credito1.calculoCredito2();
-            creditos.push(credito1);   
-        } else if(cuotaCredito==12) {
-            const credito1 = new Credito (montoCredito,cuotaCredito);
-            credito1.calculoCredito3();
-            creditos.push(credito1);   
-        } else {
-            alert("Lamentablemente, no ofrecemos esta opcion de cuotas. Recarga para volver a intentar.")
-        }
-    }
-*/
-
-
-/* for (const i of creditos){
-    console.log("El monto total a pagar es $" + i.monto.toFixed(0) +". El valor de cada cuota sera de $" + i.cuota.toFixed(0));
-} *//* Utiliza array para retornar montos en consola */
-
